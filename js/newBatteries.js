97411d56e8cc5aa44e200d1c859ce668af7e6e2d
Ivan <vanyamishenko8@gmail.com>
2025-09-16 15:23:19 +0200

    Update newBatteries.js

--- js/newBatteries.js
+++ js/newBatteries.js
@@ -8,10 +8,11 @@ const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD || 'c3Uw9Xk4QBgK3.cI',
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: 3306,
     connectionLimit: 5
+    ssl: { rejectUnauthorized: false }
 });
 
 exports.initialise = function () {
